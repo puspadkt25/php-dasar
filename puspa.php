0776fd04fe7ca0b10c18755eee16a0b16fe26a0c
puspadkt25 <puspadewikusumawati2003@gmail.com>
2024-11-07 16:29:21 +0700

    Add files via upload

---
+++ puspa.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!-- saved from url=(0030)http://localhost/phppuspa.com/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Puspa Dewi K</title>
+</head>
+<body>
+    <h1>BIODATA</h1>
+
+    
+    <table border="1" cellspacing="0" padding="15px">
+        <tbody><tr>
+            <td>NAMA</td>
+            <td> : </td>
+            <td>Puspa Dewi Kusumawati</td>
+        </tr>
+
+        <tr> 
+            <td>NIM</td>
+            <td> : </td>
+            <td>D212111012</td>
+        </tr>
+
+        <tr> 
+            <td>Alamat</td>
+            <td> : </td>
+            <td>Kota Cimahi</td>
+        </tr> 
+
+        <tr> 
+            <td>Jenis Kelamin</td>
+            <td> : </td>
+            <td>PEREMPUAN</td>
+        </tr>
+
+        <tr> 
+            <td>Umur</td>
+            <td> : </td>
+            <td>21</td>
+        </tr>
+
+        <tr> 
+            <td>Berat Badan</td>
+            <td> : </td>
+            <td>67 kg</td>
+        </tr>
+
+        <tr> 
+            <td>Tinggi Badan</td>
+            <td> : </td>
+            <td>168 cm</td>
+        </tr>
+
+        <tr> 
+            <td>Hobi</td>
+            <td> : </td>
+            <td>
+                <ul>
+                                            <li>Menonton Movie</li>
+                                            <li>Menyanyi</li>
+                                    </ul>    
+            </td>
+        </tr>
+    </tbody></table>
+
+</body></html>
