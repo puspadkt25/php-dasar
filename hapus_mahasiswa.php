8b6a30a6c18fa3a2ea74362293589c8f2849d48e
puspadkt25 <puspadewikusumawati2003@gmail.com>
2025-01-10 16:54:10 +0700

    Tugas 12

---
+++ hapus_mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['login'])) {
+        if ($_SESSION['login'] != true) {
+            header("Location: login.php");
+            exit;
+        }
+    }
+    $mysqli = new mysqli('localhost', 'root', '', 'absen');
+
+    $Nim = $_GET['nim'];
+    $delete = $mysqli->query("DELETE  FROM mahasiswa WHERE Nim='$Nim'");
+
+    if($delete) {
+        $_SESSION['success'] = true;
+        $_SESSION['message'] = 'Data Berhasil Dihapus';
+        header("Location: mahasiwa.php");
+        exit();
+    }
